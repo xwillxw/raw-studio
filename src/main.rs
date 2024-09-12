b7bd5ccd562df73c795f630946881d0b9609df87
willxw <willowgoodwin2006@gmail.com>
2024-09-12 12:38:25 +0100

    added new project function

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,27 @@ impl TrackData {
 }
 
 impl Project {
-    
+    fn new(new_name: &str, new_tempo: i32, new_tracks: &[TrackData]) -> Project {
+        return Project { 
+            name: new_name.into(),
+            tempo: new_tempo,
+            tracks: new_tracks.into()
+        }
+    }
 }
 
 fn main() {
     let main_window = MainWindow::new().unwrap();
-    let new_project = Project {
-        name: "Test Project".into(),
-        tempo: 120,
-        tracks: [
-            TrackData::new("Guitar", "1"),
-            TrackData::new("Bass", "2"),
-            TrackData::new("Drums", "3"),
-        ].into()
-    };
+    let new_tracks = [
+        TrackData::new("Guitar", "1"),
+        TrackData::new("Bass", "2"),
+        TrackData::new("Drums", "3"),
+    ];
+    let new_project = Project::new(
+        "Test Project",
+        160,
+        &new_tracks
+    );
     main_window.set_project(new_project);
     main_window.run().unwrap();
 }
